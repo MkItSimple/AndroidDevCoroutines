57a08d53d8892d26a5c6fab2b7a44f2773e01729
MkItSimple <choanania1@gmail.com>
2020-05-06 11:19:33 +0800

    b5-RunBlocking

--- app/src/main/java/com/example/androiddevcoroutines/MainActivity.kt
+++ app/src/main/java/com/example/androiddevcoroutines/MainActivity.kt
@@ -14,24 +14,67 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d(TAG, "Before runBlocking")
 
-        // this will block the main thread
-        runBlocking {
-            launch {
-                delay(3000L)
-                Log.d(TAG, "Finished IO Coroutine 1")
+        // Example 3
+        // job will still continue after cancellation
+        val job = GlobalScope.launch(Dispatchers.Default) {
+            Log.d(TAG, "Starting long  running calculation...")
+            for (i in 30..40){
+                // if you don't put this if statement it will still continue to 40 after cancellation
+                if (isActive){
+                    Log.d(TAG, "Result for i = $i: ${fib(i)}")
+                }
             }
-
-            launch {
-                delay(3000L)
-                Log.d(TAG, "Finished IO Coroutine 2")
-            }
-
-            Log.d(TAG, "Start of runBlocking")
-            delay(5000L)
-            Log.d(TAG, "End of runBlocking")
+            Log.d(TAG, "Ending long running  calculation")
         }
-        Log.d(TAG, "After runBlocking")
+
+        runBlocking {
+            delay(300L)
+            Log.d(TAG, "Canceled job!")
+            job.cancel()
+        }
+
+    }
+
+    // fibonachi
+    fun fib(n: Int): Long {
+        return if (n == 0) 0
+        else if (n == 1) 1
+        else fib(n - 1) + fib(n - 2)
     }
 }
+
+
+// Example2
+// cancel
+// if the job takes more than 5 seconds will cancel the job
+//        val job = GlobalScope.launch(Dispatchers.Default) {
+//            // will repeat 5 times whatever we put on this block
+//            repeat(10){
+//                Log.d(TAG, "Coroutine  is still working...")
+//                delay(1000L)
+//            }
+//        }
+
+//        runBlocking {
+//            delay(5000L)
+//            job.cancel()
+//            Log.d(TAG, "Main thread is continuing...")
+//        }
+
+//        //Example1
+//        //join function
+//        val job = GlobalScope.launch(Dispatchers.Default) {
+//            // will repeat 5 times whatever we put on this block
+//            repeat(10){
+//                Log.d(TAG, "Coroutine  is still working...")
+//                delay(1000L)
+//            }
+//        }
+
+
+//        //it block the Main thread until the job is finishe
+//        runBlocking {
+//            job.join()
+//            Log.d(TAG, "Main thread is continuing...")
+//        }
