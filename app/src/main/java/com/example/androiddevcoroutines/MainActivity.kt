1fce976d5f2350681120f88cd27f9baf81b1d623
MkItSimple <choanania1@gmail.com>
2020-05-06 11:25:52 +0800

    Samples 2

--- app/src/main/java/com/example/androiddevcoroutines/MainActivity.kt
+++ app/src/main/java/com/example/androiddevcoroutines/MainActivity.kt
@@ -19,19 +19,17 @@ class MainActivity : AppCompatActivity() {
         // job will still continue after cancellation
         val job = GlobalScope.launch(Dispatchers.Default) {
             Log.d(TAG, "Starting long  running calculation...")
-            for (i in 30..40){
-                // if you don't put this if statement it will still continue to 40 after cancellation
-                if (isActive){
-                    Log.d(TAG, "Result for i = $i: ${fib(i)}")
+
+            // if more than 1 seconds will cancel automatically
+            withTimeout(1000L){
+                for (i in 30..60){
+                    if (isActive){
+                        Log.d(TAG, "Result for i = $i: ${fib(i)}")
+                    }
                 }
             }
-            Log.d(TAG, "Ending long running  calculation")
-        }
 
-        runBlocking {
-            delay(300L)
-            Log.d(TAG, "Canceled job!")//
-            job.cancel()
+            Log.d(TAG, "Ending long running  calculation")
         }
 
     }
@@ -43,38 +41,3 @@ class MainActivity : AppCompatActivity() {
         else fib(n - 1) + fib(n - 2)
     }
 }
-
-
-// Example2
-// cancel
-// if the job takes more than 5 seconds will cancel the job
-//        val job = GlobalScope.launch(Dispatchers.Default) {
-//            // will repeat 5 times whatever we put on this block
-//            repeat(10){
-//                Log.d(TAG, "Coroutine  is still working...")
-//                delay(1000L)
-//            }
-//        }
-
-//        runBlocking {
-//            delay(5000L)
-//            job.cancel()
-//            Log.d(TAG, "Main thread is continuing...")
-//        }
-
-//        //Example1
-//        //join function
-//        val job = GlobalScope.launch(Dispatchers.Default) {
-//            // will repeat 5 times whatever we put on this block
-//            repeat(10){
-//                Log.d(TAG, "Coroutine  is still working...")
-//                delay(1000L)
-//            }
-//        }
-
-
-//        //it block the Main thread until the job is finishe
-//        runBlocking {
-//            job.join()
-//            Log.d(TAG, "Main thread is continuing...")
-//        }
