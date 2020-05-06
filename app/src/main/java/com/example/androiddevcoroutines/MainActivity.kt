2feae6094c567c1f783c9ce67135fe53921a063a
MkItSimple <choanania1@gmail.com>
2020-05-06 09:56:06 +0800

    B2 - Starting and Delaying Coroutine

--- app/src/main/java/com/example/androiddevcoroutines/MainActivity.kt
+++ app/src/main/java/com/example/androiddevcoroutines/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.androiddevcoroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch {
+            delay(3000L) // after 3 seconds will Log below
+            Log.d(TAG, "Coroutine hello from thread ${Thread.currentThread().name}")
+        }
+        Log.d(TAG, "Hello from thread ${Thread.currentThread().name}")
     }
 }
